f4541f06700e8780ae303dc76cd9341f5ec987f0
aarondill <aaronsacks2006@gmail.com>
2023-01-05 19:48:38 -0600

    updated eslint config

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,22 +1,46 @@
-{
-	"root": true,
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module",
-		"impliedStrict": true
+/** @type {import("@typescript-eslint/utils/dist/ts-eslint/Linter.d").Linter.Config} */
+export default {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		impliedStrict: true,
 	},
-	"env": {
-		"browser": true,
-		"node": true
+	env: {
+		browser: true,
+		node: true,
 	},
-	"plugins": ["@typescript-eslint"],
-	"rules": {
-		"@typescript-eslint/naming-convention": "warn",
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"@typescript-eslint/naming-convention": [
+			{
+				selector: "default",
+				modifiers: ["unused"],
+				leadingUnderscore: "allow",
+			},
+			{
+				selector: ["default", "variable"],
+				leadingUnderscore: "allow",
+				trailingUnderscore: "allow",
+			},
+			{
+				selector: "default",
+				format: ["camelCase"],
+			},
+			{
+				selector: "variable",
+				format: ["camelCase", "UPPER_CASE"],
+			},
+			{
+				selector: "typeLike",
+				format: ["PascalCase"],
+			},
+		],
 		"@typescript-eslint/semi": "warn",
-		"eqeqeq": "warn",
+		eqeqeq: "warn",
 		"no-throw-literal": "warn",
-		"semi": "off"
+		semi: "off",
 	},
-	"ignorePatterns": ["out", "dist", "**/*.d.ts", "node_modules"]
-}
+	ignorePatterns: ["out", "dist", "**/*.d.ts", "node_modules"],
+};
